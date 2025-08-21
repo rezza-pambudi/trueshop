830100d3fa9f969cad841206d6af1e2f02ab5a76
Rezza Agung Pambudi <141853553+rezza-pambudi@users.noreply.github.com>
2025-08-21 22:47:48 +0700

    login session

---
+++ src/app/(admin)/dashboard/(auth)/sign-in/_components/form.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ActionResult } from "@/types";
+import { SignIn } from "../lib/action";
+import { useActionState } from "react";
+
+const initialState: ActionResult = {
+  error: "",
+};
+export default function FormSignIn() {
+  const [state, formAction] = useActionState(SignIn, initialState);
+  return (
+    <form action={formAction}>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input name="password" id="password" type="password" required />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button type="submit" className="w-full">
+            Sign in
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
+}
--- src/app/(admin)/dashboard/(auth)/sign-in/page.tsx
+++ src/app/(admin)/dashboard/(auth)/sign-in/page.tsx
@@ -1,47 +1,14 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import FormSignIn from "./_components/form";
 
 export default function SignInPage() {
   return (
     <main className="w-full h-screen overflow-hidden relative">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
-              w-96 bg-white rounded-2xl shadow-xl">
-          <Card className="w-full max-w-sm">
-            <CardHeader>
-              <CardTitle className="text-2xl">Login</CardTitle>
-              <CardDescription>
-                Enter your email below to login to your account.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" required />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button className="w-full">Sign in</Button>
-            </CardFooter>
-          </Card>
-        </div>
+      <div
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+              w-96 bg-white rounded-2xl shadow-xl"
+      >
+        <FormSignIn />
+      </div>
     </main>
   );
 }
