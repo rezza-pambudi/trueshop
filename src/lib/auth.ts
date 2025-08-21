1894da22be829a8bb59c0117c30c09cb0e1e4ee2
Rezza Agung Pambudi <141853553+rezza-pambudi@users.noreply.github.com>
2025-08-21 21:01:16 +0700

    setup Lucia auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,58 @@
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import prisma from "../../lib/prisma";
+import { Lucia } from "lucia";
+import { RoleUser } from "@/generated/prisma";
+import { cache } from "react";
+import { cookies } from "next/headers";
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+    sessionCookie: {
+        expires: false,
+        attributes: {
+            secure: process.env.NODE_ENV === "production",
+        }
+    },
+
+    getUserAttributes: (attributes) => {
+        return {
+            id: attributes.id,
+            name: attributes.name,
+            email: attributes.email,
+            role: attributes.role
+        }
+    }
+})
+
+export const getUser = cache(async () => {
+	const sessionId = (await cookies()).get(lucia.sessionCookieName)?.value ?? null;
+	if (!sessionId) return null;
+	const { user, session } = await lucia.validateSession(sessionId);
+	try {
+		if (session && session.fresh) {
+			const sessionCookie = lucia.createSessionCookie(session.id);
+			(await cookies()).set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+		}
+		if (!session) {
+			const sessionCookie = lucia.createBlankSessionCookie();
+			(await cookies()).set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+		}
+	} catch {
+		// Next.js throws error when attempting to set cookies when rendering page
+	}
+	return user;
+});
+
+declare module "lucia" {
+    interface Register {
+        Lucia: typeof lucia;
+        UserId: number;
+        DatabaseUserAttributes: {
+            id: number;
+            name: string;
+            email: string;
+            role: RoleUser;
+        }
+    }
+}
